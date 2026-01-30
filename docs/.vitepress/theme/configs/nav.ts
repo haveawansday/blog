1d00fd172f3f0e723de0d854d4b93399ea58c8a0
haveawansday <tkw1004+wan@gmail.com>
2026-01-30 23:07:00 +0800

    [Refractor] - split config

---
+++ docs/.vitepress/theme/configs/nav.ts
@@ -0,0 +1,20 @@
+import { DefaultTheme } from 'vitepress';
+
+export default [
+    {
+        text: '✨️ wankorea',
+        items: [
+            { text: '學習筆記', link: '/korean/test1' },
+            { text: '學習資源', link: '/korean/K0003_korean_learning_resources_download' }
+        ]
+    },
+    {
+        text: '✨️ wandata',
+        link: '/data/data1'
+    },
+] as DefaultTheme.NavItem[];
+
+// nav: [
+//       // { text: 'Home', link: '/' },
+//       { text: 'wankorea', link: '/korean/K0003_korean_learning_resources_download' }
+//     ],
